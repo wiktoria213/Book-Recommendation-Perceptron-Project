091f632949df8ee773cd7a93a4151999cb1a4a41
wiktoria213 <wk30459@zpsb.pl>
2026-05-15 18:56:34 +0200

    Training perceptron on dataset

--- app/main.py
+++ app/main.py
@@ -22,14 +22,39 @@ users = pd.read_csv(
 ratings = ratings[ratings["Book-Rating"] != 0]
 users = users[users["Age"].notnull()]
 
-print("\nBOOKS")
-print(books.head())
-print(books.columns)
+ratings["Liked"] = ratings["Book-Rating"].apply(
+    lambda rating: 1 if rating >= 7 else -1)
 
 print("\nRATINGS")
-print(ratings.head())
-print(ratings.columns)
+print(ratings[["User-ID", "ISBN", "Book-Rating", "Liked"]].head(20))
 
-print("\nUSERS")
-print(users.head())
-print(users.columns)
+print(ratings["Liked"].value_counts())
+
+data = ratings.merge(users, on="User-ID")
+user_rating_counts = data.groupby("User-ID").size().reset_index(name="Ratings-Count")
+
+active_users = user_rating_counts[user_rating_counts["Ratings-Count"] > 1]
+
+data = data[data["User-ID"].isin(active_users["User-ID"])]
+
+user_features = users.merge(active_users, on="User-ID")
+
+print("\nUSER FEATURES")
+print(user_features[["User-ID", "Age", "Ratings-Count"]].head(20))
+X = data[["Age", "Book-Rating"]].values
+y = data["Liked"].values
+
+perceptron = PerceptronScratch(
+    lr=0.01,
+    epochs=10)
+
+perceptron.fit(X, y)
+
+print("\nWagi perceptronu:")
+print(perceptron.w)
+
+print("\nBias:")
+print(perceptron.b)
+
+print("\nBłędy w epokach:")
+print(perceptron.errors_per_epoch)
--- app/perceptron.py
+++ app/perceptron.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class PerceptronScratch:
     def __init__(self, lr=0.1, epochs=20):
         self.lr = lr
@@ -9,8 +10,25 @@ class PerceptronScratch:
         self.errors_per_epoch = []
 
     def fit(self, X, y):
-        #
+        self.w = np.zeros(X.shape[1])
+        self.b = 0.0
+        self.errors_per_epoch = []
+
+        for _ in range(self.epochs):
+            errors = 0
+            for xi, target in zip(X, y):
+                prediction = self.predict_one(xi)
+                if prediction != target:
+                    self.w = self.w + self.lr * target * xi
+                    self.b = self.b + self.lr * target
+                    errors += 1
+
+            self.errors_per_epoch.append(errors)
         return self
+
+    def predict_one(self, x):
+        score = np.dot(x, self.w) + self.b
+        return 1 if score >= 0 else -1
 
     def decision_function(self, X):
         return np.dot(X, self.w) + self.b
